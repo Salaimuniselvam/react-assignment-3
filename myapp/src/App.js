618bd27534f45e61710ac662871a1a38c35d59db
Salaimuniselvam <salaimuniselvam7@gmail.com>
2021-07-15 17:18:18 +0530

    Assignment - 2

---
+++ myapp/src/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Icon from "./Components/Icons"
+import "./style.css"
+
+const App =()=> {
+    return (
+        <div>
+            <h1>Welcome to tictactoe <Icon /></h1>
+        </div>
+    )
+}
+
+export default App
---
+++ myapp/src/Components/Icons.js
@@ -0,0 +1,10 @@
+import React from "react"
+import {FaTimes,FaRegCircle,Fapen} from "react-icons/fa"
+
+const Icon = () => {
+    return (
+        <FaTimes className="icon" />
+    )
+}
+
+export default Icon
